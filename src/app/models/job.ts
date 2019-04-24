6e6156bf2390d4b5feccc0a6f625d66e95c95320
Leonardo <leonardo.capoccia@hotmail.it>
2019-04-24 19:06:09 +0200

    Added fields to company and job

--- src/app/models/job.ts
+++ src/app/models/job.ts
@@ -1,8 +1,13 @@
+import { Company } from './company';
+
 export class Job {
   id: string;
+  name: string;
   firstVisit: Date;
   lastVisit: Date;
   link: string;
   requirements: string[];
   responsibilities: string[];
+  platform: string;
+  company: Company;
 }
